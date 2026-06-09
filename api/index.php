5a4e6c3f7b7e4b4811d1adb8dbb5fc8565804af6
1ndraabot <rayindasta@gmail.com>
2026-06-10 06:30:28 +0700

    test login register - 4.4

--- api/index.php
+++ api/index.php
@@ -7,3 +7,19 @@ echo json_encode([
     'bootstrap_exists' => file_exists(__DIR__ . '/../bootstrap/app.php'),
     'public_exists' => file_exists(__DIR__ . '/../public/index.php'),
 ]);
+
+try {
+
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+
+} catch (\Throwable $e) {
+
+    echo json_encode([
+        'bootstrap_error' => true,
+        'message' => $e->getMessage(),
+        'file' => $e->getFile(),
+        'line' => $e->getLine(),
+    ]);
+
+    exit;
+}
