804cac022541022e9124be6a761e84f9d384b38a
1ndraabot <rayindasta@gmail.com>
2026-06-10 06:27:20 +0700

    test login register - 4.3

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
-<?php  
+<?php
 
-require __DIR__ . "/../public/index.php";
+echo json_encode([
+    'php_version' => PHP_VERSION,
+    'cwd' => getcwd(),
+    'vendor_exists' => file_exists(__DIR__ . '/../vendor/autoload.php'),
+    'bootstrap_exists' => file_exists(__DIR__ . '/../bootstrap/app.php'),
+    'public_exists' => file_exists(__DIR__ . '/../public/index.php'),
+]);
